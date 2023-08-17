e9e9c302ddfe288ee68155f35fd0269ca41868f6
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-08-17 17:59:40 +0200

    deleted commented out code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+// App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../src/jsx/components/Layout.jsx';
 import Home from '../src/jsx/pages/Home.jsx';
-// import Venues from './components/Venues.jsx';
-// import VenueDetails from './pages/VenueDetails.jsx';
 
 function App() {
   return (
@@ -11,8 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="Venues" element={<Venues />} />
-          <Route path="Venue/:id" element={<VenueDetails />} /> */}
         </Route>
       </Routes>
     </React.StrictMode>
