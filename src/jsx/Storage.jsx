556f43da5a4f53408b505175a160cab93a763271
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-12 21:13:59 +0200

    updated load and save

--- src/jsx/Storage.jsx
+++ src/jsx/Storage.jsx
@@ -1,16 +1,23 @@
-
+// Storage.jsx:
 export function load(key) {
-    try {
-      const value = localStorage.getItem(key);
-      return JSON.parse(value);
-    } catch (error) {
-      console.error("Loading failed:", error);
+  try {
+    const value = localStorage.getItem(key);
+    if (key === 'token' || key === 'profile') {
+        return value;
     }
+    return JSON.parse(value);
+  } catch (error) {
+    console.error("Loading failed:", error);
   }
-  
-  export function save(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function save(key, value) {
+  if (key === 'token') {
+      localStorage.setItem(key, value);
+  } else {
+      localStorage.setItem(key, JSON.stringify(value));
   }
+}
   
   export function remove(key) {
     localStorage.removeItem(key);
