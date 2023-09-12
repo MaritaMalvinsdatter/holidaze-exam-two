2ee284345d6633d0009a642bac60aafe0d2ac931
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-12 21:12:44 +0200

    link to profile page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from '../src/jsx/components/Layout.jsx';
 import Home from '../src/jsx/pages/Home.jsx';
 import LoginForm from '../src/jsx/pages/Login.jsx';
 import RegisterForm from '../src/jsx/pages/Register.jsx';
+import ProfilePage from '../src/jsx/pages/Profile.jsx';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="login" element={<LoginForm />} /> 
+          <Route path="profile" element={<ProfilePage />} /> 
           <Route path="register" element={<RegisterForm />} /> 
         </Route>
       </Routes>
--- src/jsx/components/Nav.jsx
+++ src/jsx/components/Nav.jsx
@@ -8,6 +8,7 @@ function Nav() {
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/login">Log In</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
         {/* <li><Link to="/contact">Contact</Link></li> */}
       </ul>
     </nav>
