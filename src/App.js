a8780dfc6b71dfe61c57cea50d09486a2765e2c9
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-16 12:13:35 +0200

    added path to venuespacs + bootstrap styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// App.js
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../src/jsx/components/Layout.jsx';
@@ -7,6 +5,8 @@ import Home from '../src/jsx/pages/Home.jsx';
 import LoginForm from '../src/jsx/pages/Login.jsx';
 import RegisterForm from '../src/jsx/pages/Register.jsx';
 import ProfilePage from '../src/jsx/pages/Profile.jsx';
+import VenueDetails from '../src/jsx/pages/VenueDetails.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -15,8 +15,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="login" element={<LoginForm />} /> 
-          <Route path="profile" element={<ProfilePage />} /> 
-          <Route path="register" element={<RegisterForm />} /> 
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="register" element={<RegisterForm />} />
+          <Route path="venue/:id" element={<VenueDetails />} />
         </Route>
       </Routes>
     </React.StrictMode>
@@ -24,4 +25,3 @@ function App() {
 }
 
 export default App;
-
