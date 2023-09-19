60471b33f03e19d154f6c2f56aaec37ee6a0f2d6
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-19 20:33:02 +0200

    font awesome insalled

--- src/jsx/components/VenueList.jsx
+++ src/jsx/components/VenueList.jsx
@@ -2,10 +2,13 @@
 
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../../styles/VenueList.module.css';
 
-const url = 'https://api.noroff.dev/api/v1/holidaze/venues?limit=100';
+
+const url = 'https://api.noroff.dev/api/v1/holidaze/venues?limit=100_bookings=true&_owner=true';
 
 function Venues() {
   const [Venues, setVenues] = useState([]);
@@ -22,6 +25,7 @@ function Venues() {
         const sortedVenues = json.sort((a, b) => new Date(b.created) - new Date(a.created));// sorted by date created
         const venuesWithMedia = sortedVenues.filter(venue => venue.media && venue.media.length > 0); // only diplay venues with images
         const limitedVenues = venuesWithMedia.slice(0, 100);
+        console.log(limitedVenues);
         setVenues(limitedVenues);
         // console.log(limitedVenues);
         setIsLoading(false);
@@ -34,39 +38,65 @@ function Venues() {
     getData();
   }, []);
 
-  if (isLoading) {
-    return <div>Loading Venues</div>;
-  }
+    if (isLoading) {
+      return <div>Loading Venues</div>;
+    }
 
-  if (isError) {
-    return <div>Error loading data</div>;
-  }
+    if (isError) {
+      return <div>Error loading data</div>;
+    }
 
-  return (
-    <div>
-      {Venues.map((Venue) => (
-        <div key={Venue.id}>
-          <Link to={`/venue/${Venue.id}`}>
-            <h2>{Venue.name}</h2>
-          </Link>
-          <p>{Venue.description}</p>
-          <div>
-            <Carousel>
-              {Venue.media.map((mediaUrl, index) => (
-                <div key={index}>
-                  <img
-                    src={mediaUrl}
-                    alt={`Media ${index}`}
-                    style={{ maxWidth: '50%', height: 'auto' }}
-                  />
-                </div>
+    return (
+      <Container>
+          <Row>
+              {Venues.map((Venue) => (
+                  <Col xs={12} sm={6} md={4} lg={3} key={Venue.id}>
+                      <Card className="mb-4">
+                          {Venue.media.length > 0 && (
+                              <Card.Img 
+                                  variant="top" 
+                                  src={Venue.media[0]} 
+                                  alt="Venue Image" 
+                                  className={styles.venueListImage} 
+                              />
+                          )}
+                          <Card.Body>
+                            <Card.Title>
+                                <Link to={`/venue/${Venue.id}`}>{Venue.name}</Link>
+                            </Card.Title>
+                            
+                            <Card.Text>
+                                <i className="mb-4">
+                                    {
+                                        (Venue.location.address === "Unknown" && Venue.location.city === "Unknown") 
+                                        ? "Location is unknown"
+                                        : `${Venue.location.city}, ${Venue.location.country}`
+                                    }
+                                </i>
+                            </Card.Text>
+                            
+                            <Card.Text>
+                                Maximum {Venue.maxGuests} guests
+                                {Venue.price}NOK / Night
+                                Hosted by {Venue.owner.name}
+                            </Card.Text>
+                            
+                            <Card.Text>
+                            <i className={`fas fa-mug-saucer ${Venue.meta.breakfast ? '' : 'text-light'}`}></i>
+
+                                <i className={`fa-solid fa-square-parking ${Venue.meta.parking ? '' : 'text-light'}`}></i>
+                                <i className={`fa-solid fa-paw ${Venue.meta.pets ? '' : 'text-light'}`}></i>
+                                <i className={`fa-solid fa-wifi ${Venue.meta.wifi ? '' : 'text-light'}`}></i>
+                            </Card.Text>
+                        </Card.Body>
+
+                      </Card>
+                  </Col>
               ))}
-            </Carousel>
-          </div>
-        </div>
-      ))}
-    </div>
+          </Row>
+      </Container>
   );
 }
 
 export default Venues;
+
