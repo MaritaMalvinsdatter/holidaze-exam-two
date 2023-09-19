ed89e1da0223244f30b4e8d2054defce976e74f8
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-19 13:52:06 +0200

    user can view own bookings and venues

--- src/jsx/pages/Profile.jsx
+++ src/jsx/pages/Profile.jsx
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { API_BASE, API_PROFILE } from '../EndPoints';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button  } from 'react-bootstrap';
 import logout from '../ApiHelper';
-import { Link } from 'react-router-dom'; 
+import { Link, useNavigate } from 'react-router-dom'; 
 
 function ProfilePage() {
     const [user, setUser] = useState(null);
     const [error, setError] = useState(null);
+    const navigate = useNavigate();
+    const navigateToVenueDetails = (venueId) => {
+        navigate(`/venue/${venueId}`);
+    };
 
     useEffect(() => {
         const fetchUser = async () => {
             try {
                 const token = localStorage.getItem("token");
                 const storedProfile = JSON.parse(localStorage.getItem("profile"));
-        
-                const response = await fetch(API_BASE + API_PROFILE + storedProfile.name, {
+                const requestUrl = `${API_BASE}${API_PROFILE}${storedProfile.name}?_bookings=true&_venues=true`;
+    
+                const response = await fetch(requestUrl, {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
@@ -29,6 +34,7 @@ function ProfilePage() {
                 }
         
                 const userData = await response.json();
+                console.log(userData);
                 setUser(userData);
             } catch (error) {
                 console.error("Error fetching user data:", error);
@@ -38,6 +44,7 @@ function ProfilePage() {
     
         fetchUser();
     }, []);
+    
 
     const handleBecomeManager = async () => {
        
@@ -86,7 +93,16 @@ function ProfilePage() {
         objectFit: 'cover',
         marginBottom: '15px'
     };
-
+    
+    // Calculating price for bookings:
+    const getTotalPrice = (startDate, endDate, nightlyPrice) => {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        const differenceInTime = end.getTime() - start.getTime();
+        const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+        return differenceInDays * nightlyPrice;
+    };
+    
     return (
         <Container>
             <Row className="justify-content-center text-center">
@@ -119,7 +135,63 @@ function ProfilePage() {
                     <button onClick={logout}>Logout</button>
                 </Col>
             </Row>
+
+            <Col xs={12}>
+                <hr />
+            </Col>
+
+            <Row>
+                <Col xs={12} md={6}>
+                    <h3 className="text-center">Your Bookings:</h3>
+                    {user.bookings.map((booking) => {
+                        const totalPrice = getTotalPrice(booking.dateFrom, booking.dateTo, booking.venue.price);
+                        return (
+                            <Card key={booking.id} className="mb-3">
+                                <Card.Img variant="top" src={booking.venue.media[0] || '/path-to-default-image.jpg'} />
+                                <Card.Body>
+                                    <Card.Title>{booking.venue.name}</Card.Title>
+                                    <Card.Text>
+                                        <strong>Your Booking Dates:</strong> {new Date(booking.dateFrom).toLocaleDateString()} to {new Date(booking.dateTo).toLocaleDateString()}
+                                        <br />
+                                        <strong>Total Price:</strong> ${totalPrice}
+                                        <br />
+                                        <strong>Guests:</strong> {booking.guests}   
+                                    </Card.Text>
+                                    <Button variant="primary" onClick={() => navigateToVenueDetails(booking.venue.id)}>Details</Button>
+                                </Card.Body>
+                            </Card>
+                        );
+                    })}
+
+                </Col>
+
+                <Col xs={12} md={6} style={{ borderLeft: '1px solid #dee2e6' }}>
+                    <h3 className="text-center">Your Venues:</h3>
+                    {user.venues && user.venues.length ? (
+                        user.venues.map(venue => (
+                            <Card className="mb-3" key={venue.id}>
+                                <Card.Img variant="top" src={venue.media[0] || '/path-to-default-image.jpg'} />
+                                <Card.Body>
+                                    <Card.Title>{venue.name}</Card.Title>
+                                    <Card.Text>
+                                        {venue.description}
+                                        <strong>Max Guests:</strong> {venue.maxGuests}
+                                        <br />
+                                        <strong>Price pr Night:</strong> {venue.price}
+                                    </Card.Text>
+                                    <Button variant="primary" onClick={() => navigateToVenueDetails(venue.id)}>Details</Button>
+                                </Card.Body>
+                            </Card>
+                        ))
+                    ) : (
+                        <p>You manage no venues.</p>
+                    )}
+                </Col>
+
+            </Row>
+            
         </Container>
+        
     );
 }
 
