4ecea3265d5864c7ff8d88603f16a6a5d574017e
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-22 13:36:46 +0200

    nav updated

--- src/jsx/components/Nav.jsx
+++ src/jsx/components/Nav.jsx
@@ -1,17 +1,33 @@
-// Nav.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav as BootstrapNav, Container } from 'react-bootstrap';
+import styles from '../../styles/Nav.module.css';
 
 function Nav() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Log In</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        {/* <li><Link to="/contact">Contact</Link></li> */}
-      </ul>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" className={styles.navbar}>
+      <Container>
+        <Navbar.Brand>
+          <Link to="/" className={styles.logoLink}>
+            HOLIDAZE
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <BootstrapNav className="ms-auto">
+              <BootstrapNav.Item>
+                  <BootstrapNav.Link as={Link} to="/" className={styles.navLink}>Home</BootstrapNav.Link>
+              </BootstrapNav.Item>
+              <BootstrapNav.Item>
+                  <BootstrapNav.Link as={Link} to="/login" className={styles.navLink}>Log In</BootstrapNav.Link>
+              </BootstrapNav.Item>
+              <BootstrapNav.Item>
+                  <BootstrapNav.Link as={Link} to="/profile" className={styles.navLink}>Profile</BootstrapNav.Link>
+              </BootstrapNav.Item>
+          </BootstrapNav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
