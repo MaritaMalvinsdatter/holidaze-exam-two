83673c40fdd670536279ef2cdbb4e379e18711ad
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-27 11:23:59 +0200

    added validation to form

--- src/jsx/pages/Login.jsx
+++ src/jsx/pages/Login.jsx
@@ -1,88 +1,109 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useApiHelper } from '../ApiHelper'; // <-- Updated import
+import { Link, useNavigate } from 'react-router-dom';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { API_BASE, API_LOGIN } from '../EndPoints';
+import { useApiHelper } from '../ApiHelper'; 
 
-function LoginForm() {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+const LoginForm = () => {
+  const { saveUserAndToken } = useApiHelper(); 
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .email('Invalid email format')
+      .test(
+        'is-noroff-domain',
+        'Email must end with @noroff.no or @stud.noroff.no',
+        value => {
+          return value && (value.endsWith('@noroff.no') || value.endsWith('@stud.noroff.no'));
+        }
+      )
+      .required('Email is required'),
+    password: yup
+      .string()
+      .min(8, 'Password must be at least 8 characters')
+      .required('Password is required'),
   });
 
-  // Use the hook here instead
-  const { saveUserAndToken } = useApiHelper(); 
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      try {
+        const authenticationUrl = API_BASE + API_LOGIN;
+        const response = await fetch(authenticationUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(values),
+        });
   
-    const authenticationUrl = API_BASE + API_LOGIN;
+        if (response.ok) {
+          const data = await response.json();
+          const { name, email, avatar, venueManager, accessToken } = data;
   
-    try {
-      const response = await fetch(authenticationUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+          const userData = { name, email, avatar, venueManager };
   
-      if (response.ok) {
-        const data = await response.json();
-        const { name, email, avatar, venueManager, accessToken } = data;
+          saveUserAndToken(userData, accessToken); 
+          navigate('/profile');
   
-        const userData = { name, email, avatar, venueManager }; // Grouping the user-related data
-  
-        saveUserAndToken(userData, accessToken); // Save the user data and access token
-  
-        console.log('User logged in:', userData);
-      } else {
-        console.error('Authentication failed');
+          console.log('User logged in:', userData);
+        } else {
+          setError('Something went wrong, please try again');
+        }
+      } catch (error) {
+        console.error('Registration Error:', error);
+        setError('Something went wrong, please try again'); 
       }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-  
+    },
+  });
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <p>
-        Don't have an account yet? <Link to="/register">Register here</Link>
-      </p>
+    <div className="d-flex justify-content-center align-items-center vh-70">
+      <div className="w-50"> 
+        <h1 className="my-5 text-center">Login</h1>
+        <Form onSubmit={formik.handleSubmit}>
+          <div className="form-outline mb-4">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="e.g user@stud.noroff.no"
+              {...formik.getFieldProps('email')}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <Alert variant="danger">{formik.errors.email}</Alert>
+            ) : null}
+          </div>
+          <div className="form-outline mb-4">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              {...formik.getFieldProps('password')}
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <Alert variant="danger">{formik.errors.password}</Alert>
+            ) : null}
+          </div>
+          <Button type="submit">Login</Button>
+          {error && <Alert variant="danger">{error}</Alert>}
+        </Form>
+        <p className="mt-3 text-center">
+          Don't have an account yet? <Link to="/register">Register here</Link>
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default LoginForm;
