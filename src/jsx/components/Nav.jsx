9ea7ac79ce01aa409dd697d1f104ff531eb4ade8
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-28 12:05:43 +0200

    change nav depening on user logged in or not

--- src/jsx/components/Nav.jsx
+++ src/jsx/components/Nav.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav as BootstrapNav, Container } from 'react-bootstrap';
 import styles from '../../styles/Nav.module.css';
+import { useApiHelper, logout } from '../ApiHelper';
 
 function Nav() {
+  const { token } = useApiHelper(); 
+
+  const handleLogout = () => {
+    logout();  
+  };
   return (
     <Navbar collapseOnSelect expand="lg" className={styles.navbar}>
       <Container>
@@ -18,11 +24,19 @@ function Nav() {
               <BootstrapNav.Item>
                   <BootstrapNav.Link as={Link} to="/" className={styles.navLink}>Home</BootstrapNav.Link>
               </BootstrapNav.Item>
+              {token && (  // If user is logged in display profile
+                <BootstrapNav.Item>
+                    <BootstrapNav.Link as={Link} to="/profile" className={styles.navLink}>Profile</BootstrapNav.Link>
+                </BootstrapNav.Item>
+              )}
               <BootstrapNav.Item>
-                  <BootstrapNav.Link as={Link} to="/login" className={styles.navLink}>Log In</BootstrapNav.Link>
-              </BootstrapNav.Item>
-              <BootstrapNav.Item>
-                  <BootstrapNav.Link as={Link} to="/profile" className={styles.navLink}>Profile</BootstrapNav.Link>
+                  {token ? (
+                    // If user is logged in show log out
+                    <BootstrapNav.Link onClick={handleLogout} className={styles.navLink}>Log Out</BootstrapNav.Link>
+                  ) : (
+                    // If user is not logged in show log in
+                    <BootstrapNav.Link as={Link} to="/login" className={styles.navLink}>Log In</BootstrapNav.Link>
+                  )}
               </BootstrapNav.Item>
           </BootstrapNav>
         </Navbar.Collapse>
