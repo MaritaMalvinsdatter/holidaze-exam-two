b3590742e98ffe8d9d59d6f3c52a179ed7451cb0
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-28 08:19:24 +0200

    added usenavigate to get to login page

--- src/jsx/pages/Register.jsx
+++ src/jsx/pages/Register.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useFormik } from 'formik';
+import { useNavigate } from 'react-router-dom';
 import * as yup from 'yup';
 import { API_BASE, API_REGISTER } from '../EndPoints';
 import { apiRequest, useApiHelper } from '../ApiHelper';
 
 const RegisterForm = () => {
   const { saveUserAndToken } = useApiHelper(); 
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
 
   const validationSchema = yup.object({
     name: yup.string().required('Name is required'),
@@ -29,6 +32,7 @@ const RegisterForm = () => {
       venueManager: false,
     },
     validationSchema: validationSchema,
+    
     onSubmit: async (values) => {
         console.log('Form Values:', values);
       try {
@@ -38,14 +42,16 @@ const RegisterForm = () => {
           body: JSON.stringify(values),
         });
 
-        if (response) {
+        if (response.ok) {
           const { userData, authToken } = response;
           saveUserAndToken(userData, authToken);
+          navigate('/login');
         } else {
-          console.log('ERROR');
+          setError('Something went wrong, please try again');
         }
       } catch (error) {
         console.error('Registration Error:', error);
+        setError('Something went wrong, please try again'); 
       }
     },
   });
@@ -55,64 +61,65 @@ const RegisterForm = () => {
       <div className="w-50"> 
       <h1 className="my-5 text-center">Register New User:</h1>
         <Form onSubmit={formik.handleSubmit}>
+              <div className="form-outline mb-4">
+                <Form.Label>Name:</Form.Label>
+                <Form.Control
+                type="text"
+                name="name"
+                placeholder="Write your name"
+                {...formik.getFieldProps('name')}
+              />
+              {formik.touched.name && formik.errors.name ? (
+              <Alert variant="danger">{formik.errors.name}</Alert>
+            ) : null}
+          </div>
           <div className="form-outline mb-4">
-            <Form.Label>Name:</Form.Label>
+            <Form.Label>Email:</Form.Label>
             <Form.Control
-            type="text"
-            name="name"
-            placeholder="Write your name"
-            {...formik.getFieldProps('name')}
-          />
-          {formik.touched.name && formik.errors.name ? (
-          <Alert variant="danger">{formik.errors.name}</Alert>
-        ) : null}
-      </div>
-      <div className="form-outline mb-4">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          placeholder="Enter your email"
-          {...formik.getFieldProps('email')}
-        />
-        {formik.touched.email && formik.errors.email ? (
-          <Alert variant="danger">{formik.errors.email}</Alert>
-        ) : null}
-      </div>
-      <div className="form-outline mb-4">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-          {...formik.getFieldProps('password')}
-        />
-        {formik.touched.password && formik.errors.password ? (
-          <Alert variant="danger">{formik.errors.password}</Alert>
-        ) : null}
-      </div>
-      <div className="form-outline mb-4">
-        <Form.Label>Avatar URL (Optional):</Form.Label>
-        <Form.Control
-          type="url"
-          name="avatar"
-          placeholder="Enter your avatar URL"
-          {...formik.getFieldProps('avatar')}
-        />
-        {formik.touched.avatar && formik.errors.avatar ? (
-          <Alert variant="danger">{formik.errors.avatar}</Alert>
-        ) : null}
-      </div>
-      <div className="form-check mb-4">
-        <Form.Check 
-         type="checkbox"
-          label="Register as Venue Manager"
-          name="venueManager"
-          {...formik.getFieldProps('venueManager')}
-        />
-      </div>
-      <Button type="submit">Register</Button>
-    </Form>
+              type="email"
+              name="email"
+              placeholder="Enter email ending in @stud.noroff.no or @noroff.no"
+              {...formik.getFieldProps('email')}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <Alert variant="danger">{formik.errors.email}</Alert>
+            ) : null}
+          </div>
+          <div className="form-outline mb-4">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Enter password, minimum 8 characthers"
+              {...formik.getFieldProps('password')}
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <Alert variant="danger">{formik.errors.password}</Alert>
+            ) : null}
+          </div>
+          <div className="form-outline mb-4">
+            <Form.Label>Avatar URL (Optional):</Form.Label>
+            <Form.Control
+              type="url"
+              name="avatar"
+              placeholder="Enter your avatar URL"
+              {...formik.getFieldProps('avatar')}
+            />
+            {formik.touched.avatar && formik.errors.avatar ? (
+              <Alert variant="danger">{formik.errors.avatar}</Alert>
+            ) : null}
+          </div>
+          <div className="form-check mb-4">
+            <Form.Check 
+            type="checkbox"
+              label="Register as Venue Manager"
+              name="venueManager"
+              {...formik.getFieldProps('venueManager')}
+            />
+          </div>
+          <Button type="submit">Register</Button>
+          {error && <Alert variant="danger">{error}</Alert>}
+        </Form>
       </div>
     </div>
     
