954c6dedfc778b25bddabcde4c8fc43ac3622a6f
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-29 12:06:17 +0200

    moved nav directly into layout, header un-used

--- src/jsx/components/Layout.jsx
+++ src/jsx/components/Layout.jsx
@@ -1,13 +1,13 @@
 // Layout.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './Header.jsx';
+import Nav from './Nav.jsx';
 import Footer from './Footer.jsx';
 
 function Layout() {
   return (
     <div>
-      <Header />
+      <Nav />
       <Outlet />
       <Footer />
     </div>
