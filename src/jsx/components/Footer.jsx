6dbda37654716f514539c524145f862360188d0a
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-09-29 15:32:33 +0200

    footer txt centred, padding added

--- src/jsx/components/Footer.jsx
+++ src/jsx/components/Footer.jsx
@@ -1,11 +1,14 @@
 // Footer.jsx
 import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 
 function Footer() {
   return (
-    <footer>
-      <p>Holidaze &copy; {new Date().getFullYear()} </p>
-    </footer>
+    <Navbar className="border-top" style={{ backgroundColor: '#f8f9fa' }}>
+      <Container className="text-center p-5 justify-content-center">
+        <p className="mb-0">Holidaze &copy; {new Date().getFullYear()}</p>
+      </Container>
+    </Navbar>
   );
 }
 
